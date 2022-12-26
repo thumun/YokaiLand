548aeff440e7667ca416812c0a74ea163f8cd511
nehathumu <nthumu@haverford.edu>
2022-12-26 12:48:25 -0500

    adding assets

---
+++ Unity.YokaiLand/Assets/Scripts/TitleCard.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleCard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // start panel & buttons are active 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // when start button is pressed start the game
+
+        // when end button is pressed end (?) -- how to quit 
+    }
+}
